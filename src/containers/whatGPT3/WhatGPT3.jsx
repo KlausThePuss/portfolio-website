9bd0c165675186bfe8e46586fc13fdee0925bae6
Klaus <klausthepuss@gmail.com>
2023-06-03 14:58:48 -0300

    Features done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import "./whatGPT3.css";
 import { Feature } from "../../components";
 
+const featuresWhatGPT3Data = [
+    {
+        title: "Chatbots",
+        text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought.",
+    },
+    {
+        title: "Knowledgebase",
+        text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments",
+    },
+    {
+        title: "Education",
+        text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments",
+    },
+];
+
 const WhatGPT3 = () => {
     return (
         <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
@@ -15,23 +30,16 @@ const WhatGPT3 = () => {
                 <h1 className="gradient__text">
                     The possibilities are beyond your imagination
                 </h1>
-                <ul>
-                    <p>Explore the Library</p>
-                </ul>
+                <p>Explore the Library</p>
             </div>
             <div className="gpt3__whatgpt3-container">
-                <Feature
-                    title="Chatbots"
-                    text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
-                />
-                <Feature
-                    title="Knowledgebase"
-                    text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
-                />
-                <Feature
-                    title="Education"
-                    text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
-                />
+                {featuresWhatGPT3Data.map((item, index) => (
+                    <Feature
+                        title={item.title}
+                        text={item.text}
+                        key={item.titte + index}
+                    />
+                ))}
             </div>
         </div>
     );
