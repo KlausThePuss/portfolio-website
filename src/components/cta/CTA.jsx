944c91bc11df559b49f32996d708282dae2c4369
Klaus <klausthepuss@gmail.com>
2023-06-05 15:52:39 -0300

    #features done, #possibility done, CTA done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-    return <div>CTA</div>;
+    return (
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <div className="gpt3__cta-content_xd">
+                    <h1>Request Early Access to Get Started</h1>
+                    <p>
+                        Register Today & start exploring the endless
+                        possibilities.
+                    </p>
+                </div>
+                <div className="gpt3__cta-content_button">
+                    <button type="button">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CTA;
